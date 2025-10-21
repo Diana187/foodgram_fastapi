57011f987cba39b4ddf66ef5510c8da06404a000
Диана Амирханова <dinaami187@gmail.com>
2025-10-21 15:04:36 +0300

    session.py edited

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,5 @@
-# подключение к БД и управление соединением (сессии)
+# подключение к БД и управление соединением
 from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager
 
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
@@ -14,15 +13,10 @@ from app.core.config import settings
 engine: AsyncEngine = create_async_engine(
     settings.db_url,
     echo=settings.DEBUG,
-    pool_pre_ping=True,
     future=True,
 )
-
-
 async_session: async_sessionmaker[AsyncSession] = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-    autoflush=False,
+    bind=engine, expire_on_commit=False, autoflush=False
 )
 
 
@@ -33,10 +27,3 @@ async def get_session() -> AsyncIterator[AsyncSession]:
 
 async def close_engine() -> None:
     await engine.dispose()
-
-
-# --- Опционально: контекстный менеджер для задач/скриптов ---
-@asynccontextmanager
-async def lifespan_session() -> AsyncIterator[AsyncSession]:
-    async with async_session() as session:
-        yield session
